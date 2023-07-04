f93cbf5d4a24ceeaf6a911c9e5b4bc26ea0b0df3
HanzalaAqeel <connect.hanzala@gmail.com>
2023-07-04 15:44:52 +0500

    Responsive-Navbar

---
+++ javascript/functioning.js
@@ -0,0 +1,9 @@
+// $(function () {
+//   getElementsById("menu").click(function () {
+//     getElementsById("ul").toggleClass("active");
+//   });
+// });
+
+$("#menu").click(function () {
+  $("ul").toggleClass("active");
+});
